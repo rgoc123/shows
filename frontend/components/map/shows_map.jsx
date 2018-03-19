50a79d449dbc091cdb6e521e6fe351a57766c3ea
Robert O'Connor <robertgoconnor2@gmail.com>
2018-03-19 17:37:24 -0400

    Create Google map framework

---
+++ frontend/components/map/shows_map.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class ShowsMap extends React.Component {
+
+  componentDidMount() {
+
+    const mapOptions = {
+      center: { lat: 40.759147, lng: -73.9802785 },
+      zoom: 12
+    };
+
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+
+    this.MarkerManager = new MarkerManager(this.map);
+    this.props.businesses.forEach(biz => {
+      this.MarkerManager.createMarkerFromBusiness(biz);
+    });
+    // this.MarkerManager.updateMarkers(this.props.businesses);
+
+    google.maps.event.addListener(this.map, 'idle', () => {
+
+      const { north, south, east, west } = this.map.getBounds().toJSON();
+
+      const bounds = {
+        northEast: { lat: north, lng: east },
+        southWest: { lat: south, lng: west }
+      };
+
+    });
+  }
+
+  render() {
+    return (
+      <div id="map-container" style={{position: 'fixed'}} ref={ map => this.mapNode = map }></div>
+    );
+  }
+
+}
+
+export default ShowsMap;
--- frontend/components/shows/shows_index.jsx
+++ frontend/components/shows/shows_index.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
 import ShowsIndexItem from './shows_index_item';
+import ShowsMap from '../map/shows_map';
 
 class ShowsIndex extends React.Component {
   componentDidMount() {
     this.props.fetchShows();
   }
 
-  render() {
+  createIndex() {
     return this.props.shows.map(show => {
       return <ShowsIndexItem key={show.id} show={show} />;
     });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.createIndex()}
+        <ShowsMap />
+      </div>
+    );
   }
 }
 
