33fd77e48944aaed6721c66d9aa256bbe4347eaf
Robert O'Connor <robertgoconnor2@gmail.com>
2018-03-22 13:11:28 -0400

    Fix shows index times timezone

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ b = Venue.create!(name: "Session 73", address: "1359 1st Ave", city: "New York",
 c = Venue.create!(name: "Five Mile Stone", address: "1640 2nd Ave", city: "New York", state: "NY", zip: "10075", borough: "Manhattan", neighborhood: "Upper East Side", lat: 40.777241, long: -73.951947, category: "Bar")
 
 Show.destroy_all
-Show.create!(artist: "Fall Out Boy", genre: "Emo", date: "1/1/2018", venue: a.name, venue_id: a.id, start_time: "02:00 AM", end_time: "07:00 AM")
-Show.create!(artist: "Fall Out Boy", genre: "Emo", date: "2/15/2018", venue: b.name, venue_id: b.id, start_time: "02:00 AM", end_time: "07:00 AM")
-Show.create!(artist: "Red Hot Chili Peppers", genre: "Rock", date: "3/17/2018", venue: c.name, venue_id: c.id, start_time: "12:00 PM", end_time: "05:00 PM")
-Show.create!(artist: "Red Hot Chili Peppers", genre: "Rock", date: "4/20/2018", venue: a.name, venue_id: a.id, start_time: "12:00 PM", end_time: "05:00 PM")
-Show.create!(artist: "Grouplove", genre: "Rock", date: "5/5/2018", venue: b.name, venue_id: b.id, start_time: "05:00 PM", end_time: "10:00 PM")
-Show.create!(artist: "Grouplove", genre: "Rock", date: "7/5/2018", venue: c.name, venue_id: c.id, start_time: "05:00 PM", end_time: "10:00 PM")
+Show.create!(artist: "Fall Out Boy", genre: "Emo", date: "1/1/2018", venue: a.name, venue_id: a.id, start_time: "02:00 AM EDT", end_time: "07:00 AM EDT")
+Show.create!(artist: "Fall Out Boy", genre: "Emo", date: "2/15/2018", venue: b.name, venue_id: b.id, start_time: "02:00 AM EDT", end_time: "07:00 AM EDT")
+Show.create!(artist: "Red Hot Chili Peppers", genre: "Rock", date: "3/17/2018", venue: c.name, venue_id: c.id, start_time: "12:00 PM EDT", end_time: "05:00 PM EDT")
+Show.create!(artist: "Red Hot Chili Peppers", genre: "Rock", date: "4/20/2018", venue: a.name, venue_id: a.id, start_time: "12:00 PM EDT", end_time: "05:00 PM EDT")
+Show.create!(artist: "Grouplove", genre: "Rock", date: "5/5/2018", venue: b.name, venue_id: b.id, start_time: "05:00 PM EDT", end_time: "10:00 PM EDT")
+Show.create!(artist: "Grouplove", genre: "Rock", date: "7/5/2018", venue: c.name, venue_id: c.id, start_time: "05:00 PM EDT", end_time: "10:00 PM EDT")
