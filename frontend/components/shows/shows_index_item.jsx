c700136ce8714c06c4307808267c4cca480c64ed
Robert O'Connor <robertgoconnor2@gmail.com>
2018-03-22 12:57:47 -0400

    Format shows index times

--- frontend/components/shows/shows_index_item.jsx
+++ frontend/components/shows/shows_index_item.jsx
@@ -7,13 +7,35 @@ class ShowsIndexItem extends React.Component {
     this.show = this.props.show;
   }
 
+  amPM(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    let newTime = hours + ":" + minutes + " " + ampm;
+    return newTime;
+  }
+
+  startTime() {
+    let startTime = new Date(this.show.start_time);
+    return this.amPM(startTime);
+  }
+
+  endTime() {
+    let endTime = new Date(this.show.end_time);
+    return this.amPM(endTime);
+  }
+
   render() {
+    debugger
     return (
       <div className="shows-index-item">
         <h2>{this.show.artist}</h2>
         <span>{this.show.venue.name}</span>
         <span>{this.show.date}</span>
-        <span>{this.show.start_time} - {this.show.end_time}</span>
+        <span>{this.startTime()} - {this.endTime()}</span>
         <span>{this.show.genre}</span>
       </div>
     );
