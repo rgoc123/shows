158a695ddeba773e4ab7a2dc23674cc816656fd4
Robert O'Connor <robertgoconnor2@gmail.com>
2018-04-18 18:36:35 -0400

    Comment out show association

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ApplicationRecord
   validates :artist, :genre, :date, :venue, :venue_id, presence: true
 
 
-  belongs_to :venue
+  # belongs_to :venue
 
   def now?
     self.start_time.localtime <= Time.now && self.end_time.localtime > Time.now
